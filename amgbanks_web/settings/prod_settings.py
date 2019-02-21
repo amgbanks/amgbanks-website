af0cc21e386db8574ff0e5db4e5d45c747b48d56
amgbanks <amgbanks@gmail.com>
2019-02-21 12:28:00 -0500

    Changed storage backends

--- amgbanks_web/settings/prod_settings.py
+++ amgbanks_web/settings/prod_settings.py
@@ -25,4 +25,4 @@ AWS_LOCATION = 'static'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATIC_URL = "https://{}/{}/".format(AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
-DEFAULT_FILE_STORAGE = 'amgbanks_web.storage_backends.MediaStorage'
+DEFAULT_FILE_STORAGE = 'amgbanks_web.settings.storage_backends.MediaStorage'
